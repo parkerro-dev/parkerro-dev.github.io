b2967a49ae98ea4f975a3d95eaf171ba14be5c2f
parkerro <parkkero.devs@gmail.com>
2022-07-30 15:47:32 +0100

    college notes added

---
+++ src/styles/notesTheme.js
@@ -0,0 +1,32 @@
+import { extendTheme } from "@chakra-ui/react";
+import {mode} from "@chakra-ui/theme-tools"
+
+
+
+const styles = {
+    global: props => ({
+        body: { bg: mode('#FFFFFF','#202020')(props)},
+       })
+
+
+}
+
+
+const config = {
+    initialColorMode: 'dark',
+    useSystemColorMode: true
+  }
+
+const fonts = {
+    heading: "'Patua One'"
+}
+
+export const notesTheme = extendTheme({config, 
+ styles, fonts, components: {
+    Heading: {
+        baseStyle:{
+        color: 'red'
+    }},
+    Link: {
+        baseStyle:{color: 'blue'}
+    }}})
